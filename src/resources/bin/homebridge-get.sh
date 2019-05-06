def2d5915994c4c60f616224f266e5c7cd6b02e4
alexelec <alexelecv5@gmail.com>
2019-05-06 13:04:02 +0300

    homebridge: fix homebridge-get.sh

--- src/resources/bin/homebridge-get.sh
+++ src/resources/bin/homebridge-get.sh
@@ -26,8 +26,8 @@ UPD_VER=`curl -s "$URL_LAST" | sed 's|.*tag\/||; s|">redirected.*||')`
   elif [ "$1" == "install" ] ; then
       rm -fR $HBR_DIR
       mkdir -p $HBR_DIR
-      tar -jxf $TEMP_DIR/homebridge-$UPD_VER.tar.bz2 -C $HBR_DIR
-      rm -f $TEMP_DIR/homebridge-$UPD_VER.tar.bz2
+      tar -jxf $TMP_DIR/homebridge-$UPD_VER.tar.bz2 -C $HBR_DIR
+      rm -f $TMP_DIR/homebridge-$UPD_VER.tar.bz2
 
   fi
 
